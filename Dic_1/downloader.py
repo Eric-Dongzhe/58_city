e5ac171c42b423ca2cfad1de41f5d922101f0da6
Eric-Dong <dong_chx@hotmail.com>
2016-10-31 23:15:16 +0800

    创建中

---
+++ Dic_1/downloader.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import time
+# from multiprocessing import Process, Queue
+from Queue import Queue
+import threading
+from mongo_cache import MongoCache
+import pymongo
+import requests
+from bs4 import BeautifulSoup
+import sqlite3
+from channel_extract import channels_string
+
+
+
+client = pymongo.MongoClient('localhost', 27017)
+DB_URL = client['DB_URL']  # Database
+url_list = DB_URL['channel_link']  # Table url_list
+
+DB_Cache = client['DB_Cache']
+channel_page = DB_Cache['channel_page']
+
+
+
+
+ExitFlag = 0
+
+
+class MyThread(threading.Thread):
+    def __init__(self, threadID, name, q):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.q = q
+
+    def run(self):
+        print "Starting " + self.name
+        get_all_page_from(self.name, self.q)
+        print "Exiting " + self.name
+
+
+def download(channel, pages, who_sells=0):
+    list_view = '{}{}/pn{}'.format(channel, str(who_sells), str(pages))
+    wb_data = requests.get(list_view)
+    record = {'result': Binary(zlib.compress(pickle.dumps(wb_data))),
+              'channel': channel,
+              'timestamp': datetime.utcnow()
+              }
+    channel_page.update({'_id': list_view}, {'$set': record}, upsert=True)
+    return list_view
+
+
+def put_url_to_queue(url_queue, channel__list):
+    for channel in channel_list:
+        url_queue.put(channel)
+        print('put channel Link queue size is %d' % (url_queue.qsize(),))
+
+
+def get_all_page_from(thread_name, queue):
+    while not ExitFlag:
+        # queueLock.acquire()
+        if not Channel_Queue.empty():
+            channel_for_parse = queue.get()
+            # queueLock.release()
+            for page in xrange(1, 5):
+                channel_list_url = download(channel_for_parse, page)
+                print "%s processing %s" % (thread_name, channel_list_url)
+                # Log_thread.write("----%s processing %s----\n" % (threadName, channel_list))
+        else:
+            pass  # queueLock.release()
+        # time.sleep(1)
+
+
+if __name__ == '__main__':
+    channel_list = channels_string.split()
+    threadList = ["Thread-1", "Thread-2", "Thread-3", "Thread-4", "Thread-5", "Thread-6", "Thread-7", "Thread-8"]
+    Channel_Queue = Queue(10)
+    queueLock = threading.Lock()
+    threads = []
+    threadID = 1
+
+    # 创建新线程
+    for tName in threadList:
+        thread = MyThread(threadID, tName, Channel_Queue)
+        thread.start()
+        threads.append(thread)
+        threadID += 1
+
+    # 填充队列
+    queueLock.acquire()
+    for channel_url in channel_list:
+        Channel_Queue.put(channel_url)
+    queueLock.release()
+
+    # 等待队列清空
+    while not Channel_Queue.empty():
+        pass
+
+    # 通知线程是时候退出
+    ExitFlag = 1
+
+    # 等待所有线程完成
+    for t in threads:
+        t.join()
+    print "Exiting Main Thread"
